913d5439610101d49dab0120ba95ee8f42efcf7f
harimrim <kigkfla@naver.com>
2023-03-06 14:45:33 +0900

    프로젝트 생성

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by 김하림 on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
