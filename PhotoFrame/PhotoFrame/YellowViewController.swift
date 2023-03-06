b13b9e2706290f8b288f8f03c904dd0f74128cc1
Hyejeong Park <effie.thedev@gmail.com>
2023-03-06 15:31:39 +0900

    [step 5] ViewController 연결하기
    - YellowVC 클래스 추가하고 연결
    - 테스트

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,17 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by Effie on 2023/03/06.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print(self, #function)
+    
+  }
+}
