593221e338de1c2c0b71480e1b8dcb4eb93d93ef
apple <sol@appleui-noteubug.local>
2023-03-06 16:01:16 +0900

    add app project

---
+++ PhotoFrame/PhotoFrame/PhotoFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFrameApp.swift
+//  PhotoFrame
+//
+//  Created by apple on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
