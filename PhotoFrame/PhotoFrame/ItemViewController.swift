f307cbce608650052195dd02f42b136d5b5bc5d9
SONG <geun2121@gmail.com>
2023-03-06 17:49:05 +0900

    Auto stash before rebase of "origin/main"
    - remote repo 동기화

---
+++ PhotoFrame/PhotoFrame/ItemViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ItemViewController.swift
+//  PhotoFrame
+//
+//  Created by SONG on 2023/03/06.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by SONG on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+}
+
