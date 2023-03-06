e0892284d284e5048ac8a9c957ae99a3eed01fd2
wnsqhs <wnsqhs7804@gmail.com>
2023-03-06 15:55:49 +0900

    미션 1~3

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by PJB on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrame/ViewController2.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController2.swift
+//  PhotoFrame
+//
+//  Created by PJB on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    @IBOutlet weak var firstDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.photoLabel.text = "JK의 사진액자"
+        self.photoLabel.textColor = .red
+        self.photoLabel.backgroundColor = .yellow
+        self.photoLabel.alpha = 0.5
+        self.photoLabel.font = UIFont.systemFont(ofSize: 32)
+        
+        
+        self.firstDescription.text = "굿굿"
+        self.firstDescription.textColor = .green
+        self.firstDescription.backgroundColor = .gray
+        self.firstDescription.alpha = 0.5
+        self.firstDescription.font = UIFont.systemFont(ofSize: 32)
+    }
+    
+
+    @IBAction func nextButtonTouched(_ sender: UIButton) {
+        
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = .yellow
+        self.photoLabel.alpha = 0.0
+    }
+    
+}
