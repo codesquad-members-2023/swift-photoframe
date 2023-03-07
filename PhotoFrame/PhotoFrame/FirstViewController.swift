79b4667f3f14af8aaaaf762e3fab1ccbd7a0789b
SONG <geun2121@gmail.com>
2023-03-07 11:26:36 +0900

    Code about Label
    - Change label's properties

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstView: UILabel!
+    
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.firstView.text = "1번 액자"
+        self.firstView.textColor = .red
+        self.firstView.alpha = 1.0
+        
+        self.firstDescription.text = "loaded by FirstViewController"
+        self.firstDescription.textColor = .red
+        self.firstDescription.alpha = 0.5
 
     }
-
 }
