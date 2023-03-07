918abe8fbdfae4d1a9cfa40f99530b4920e35e76
SONG <geun2121@gmail.com>
2023-03-07 10:58:18 +0900

    Change name that connected to scenes
    - ViewController -> FirstViewController
    - ItemViewController -> SecondViewController

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by SONG on 2023/03/07.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by SONG on 2023/03/07.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
