fb9946f15f9c733f952ac94e9af55338ee11d12c
Hyejeong Park <effie.thedev@gmail.com>
2023-03-08 11:38:42 +0900

    [fix] 피드백 반영
    - 템플릿으로 생성된 코드 중 불필요한 부분을 제거.
    - ViewController life cycle 메소드에 주석 추가.

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  /// 첫번째 화면이 메모리에 로드될 때 호출됩니다.
+  ///
+  /// 함수와 관련된 정보, callee의 정보(file, line, column), caller의 정보
   override func viewDidLoad() {
     super.viewDidLoad()
     print(#file, #line, #function, #column)
