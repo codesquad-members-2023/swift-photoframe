3a1efd9d9c0ea003f6b22fd40440dbd74318dabb
SONG <geun2121@gmail.com>
2023-03-08 15:41:13 +0900

    Change class that connected to scene
    - 기존의 ViewController, ItemViewController 삭제
    - Item1ViewController , Item2ViewController 생성 후 각 scene에 연결

---
+++ PhotoFrame/PhotoFrame/Item1ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Item1ViewController.swift
+//  PhotoFrame
+//
+//  Created by SONG on 2023/03/08.
+//
+
+import UIKit
+
+class Item1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ PhotoFrame/PhotoFrame/Item2ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Item2ViewController.swift
+//  PhotoFrame
+//
+//  Created by SONG on 2023/03/08.
+//
+
+import UIKit
+
+class Item2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
