d3d08b22e6e539a73c7bf310f9f5a8601ed56b23
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-03-08 16:11:12 +0900

    레이블 다른 방식으로 생성후 연결

--- PhotoFrame/PhotoFrame/FirstScene.swift
+++ PhotoFrame/PhotoFrame/FirstScene.swift
@@ -2,11 +2,13 @@ import UIKit
 
 class FirstScene: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
         self.firstLabel.text = "JK의 사진액자"
+        
     }
 }
 
