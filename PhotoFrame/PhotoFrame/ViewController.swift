6fec31dd608c0b0ac189c51b17f63b08cdabc8dd
HansolWorld <hs9269@gmail.com>
2023-03-08 16:05:10 +0900

    add label

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         print(#file, #line, #function, #column)
+        
+        self.firstLabel.text = "Sol의 액자"
     }
 
 
