f9f26eca55bbc8613f73f237e79d202cefcbc8f6
Haena <ghkwlsspdl@naver.com>
2023-03-08 16:22:38 +0900

    firstDescription 속성변경(코드로)

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -11,13 +11,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var firstView: UILabel!
     
+    @IBOutlet weak var firstDescription: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(#file, #line, #function, #column)
         
+        //firstView
         self.firstView.textColor = self.firstView.textColor.withAlphaComponent(0.5)
+        
+        //firstDescription
+        firstDescription.text = "밑에있지롱"
+        firstDescription.textColor = .blue
+        firstDescription.font = .systemFont(ofSize: 10)
+        firstDescription.backgroundColor = .orange
     }
 
 
