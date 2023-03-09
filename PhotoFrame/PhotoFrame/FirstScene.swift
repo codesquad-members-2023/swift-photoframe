978a8f53a45f2d503f3d2e6e731229e1d07bc08c
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-03-09 11:48:45 +0900

    FirstScene VC에 설명 주석 추가

--- PhotoFrame/PhotoFrame/FirstScene.swift
+++ PhotoFrame/PhotoFrame/FirstScene.swift
@@ -3,12 +3,12 @@ import UIKit
 class FirstScene: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var firstLabel: UILabel!
     @IBOutlet weak var firstDescription: UILabel!
+    // 새로운 연결 방법으로 레이블을 뷰컨과 연결.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
         self.firstLabel.text = "JK의 사진액자"
-        
     }
 }
 
