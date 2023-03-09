a3dcbfef1942b978910888617c24061f18e5fe88
HansolWorld <hs9269@gmail.com>
2023-03-09 14:49:23 +0900

    add print

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -15,12 +15,33 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         print(#file, #line, #function, #column)
         
         self.firstTitle.text = "Sol의 액자"
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    
+    
     @IBAction func nextButtonTouched(_ sender: UIButton) {
         self.firstTitle.textColor = UIColor.blue
         self.firstTitle.backgroundColor = UIColor.yellow
