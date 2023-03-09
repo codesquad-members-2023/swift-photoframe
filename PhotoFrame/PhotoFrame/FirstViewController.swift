7b9af3a829856fb21b199084626de63c22d327a8
Hyejeong Park <effie.thedev@gmail.com>
2023-03-09 11:08:37 +0900

    [fix] 피드백 반영
    - 구조를 고려해서 label 이름을 수정했습니다.

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,26 +9,26 @@ import UIKit
 
 class FirstViewController: UIViewController {
   
-  @IBOutlet weak var firstTitleLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
   
-  @IBOutlet weak var firstDescriptionLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    firstTitleLabel.text = "Photo Album"
-    firstTitleLabel.textColor = .lightGray
+    titleLabel.text = "Photo Album"
+    titleLabel.textColor = .lightGray
     
-    let font = UIFont.italicSystemFont(ofSize: firstDescriptionLabel.font.pointSize)
+    let font = UIFont.italicSystemFont(ofSize: descriptionLabel.font.pointSize)
     let attribute: [NSAttributedString.Key: Any] = [.font: font]
     let aString = NSAttributedString(string: "by Effie @CodeSquad", attributes: attribute)
-    firstDescriptionLabel.attributedText = aString
+    descriptionLabel.attributedText = aString
   }
   
   @IBAction func nextButtonTouched(_ sender: Any) {
-    firstTitleLabel.textColor = UIColor.blue
-    firstTitleLabel.backgroundColor = UIColor.yellow
-    firstTitleLabel.alpha = 0.5
+    titleLabel.textColor = UIColor.blue
+    titleLabel.backgroundColor = UIColor.yellow
+    titleLabel.alpha = 0.5
   }
   
 }
