dc9010f0952f3a06e6628db1bf939aa7cc8022aa
Haena <ghkwlsspdl@naver.com>
2023-03-09 15:59:16 +0900

    ViewController 연결하기
    각각의 뷰컨트롤러와 스토리보드를 연결 한 후 Assistant 편집기를 이용해 Automatic 설정으로 바꾸고, 닫기버튼을 누르면 뒤로가게 되는 함수를 만들었다.

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RedViewController.swift
+//  PhotoFrame
+//
+//  Created by leehwajin on 2023/03/09.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
