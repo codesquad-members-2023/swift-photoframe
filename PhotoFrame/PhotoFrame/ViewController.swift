c5e7ab7d59a711c45e36d52af886819a54b697b3
harimrim <kigkfla@naver.com>
2023-03-09 12:20:04 +0900

    First View Label 속성 변경

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var photoLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#file, #line, #function, #column)
+        self.photoLabel.text = "하림의 사진액자"
+        self.photoLabel.textColor = .red
+        self.photoLabel.textColor = self.photoLabel.textColor.withAlphaComponent(0.5)
+        self.photoLabel.font = self.photoLabel.font.withSize(25.0)
+        self.photoLabel.sizeToFit()
+        self.photoLabel.backgroundColor = .cyan
+        
     }
 
 
