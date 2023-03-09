19c39a960ee5508f048eb5ec692ae3502e714352
demiiann <tkim0156@gmail.com>
2023-03-10 00:05:21 +0900

    feat : 인터페이스 빌더 사용을 위한 기존 코드 삭제

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -19,8 +19,5 @@ class FirstViewController: UIViewController {
         self.photoLabel.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
         self.photoLabel.font = UIFont.systemFont(ofSize: 15)
         self.photoDescription.text = "사진에 대한 설명 Label"
-        self.photoDescription.textColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).withAlphaComponent(0.5)
-        self.photoDescription.backgroundColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
-        self.photoDescription.font = UIFont.systemFont(ofSize: 20)
     }
 }
