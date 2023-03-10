59c13db552914b7a191b226afa4351ab4e36574d
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-03-10 21:30:51 +0900

    Label 이름 수정[1-3 피드백]

--- PhotoFrame/PhotoFrame/FirstTabViewController.swift
+++ PhotoFrame/PhotoFrame/FirstTabViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 class FirstTabViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var firstLabel: UILabel!
-    @IBOutlet weak var firstDescription: UILabel!
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var firstTabDescription: UILabel!
     // 기존의 연결방식이 작동되지 않는 경우가 있어 새로운 연결방식을 학습 후 연결하여 해결함
     // 1. 먼저 버튼 / 레이블 코드를 작성
     // 2. 좌측에 생기는 연결 동그라미로 스토리보드에 있는 요소로 드래그 앤 드롭
