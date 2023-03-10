722ac7f51354a5ef1d48ab69850abc2537b4ef08
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-03-10 11:11:46 +0900

    다음 버튼 추가 기능 구현

--- PhotoFrame/PhotoFrame/FirstView.swift
+++ PhotoFrame/PhotoFrame/FirstView.swift
@@ -6,6 +6,7 @@ class FirstView: UIViewController, UITextFieldDelegate {
     // 기존의 연결방식이 작동되지 않는 경우가 있어 새로운 연결방식을 학습 후 연결하여 해결함
     // 1. 먼저 버튼 / 레이블 코드를 작성
     // 2. 좌측에 생기는 연결 동그라미로 스토리보드에 있는 요소로 드래그 앤 드롭
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,10 +14,18 @@ class FirstView: UIViewController, UITextFieldDelegate {
         self.firstLabel.text = "JK의 사진액자"
     }
     
+    // 버튼을 누르고 한번 더 누를 시 원래 레이블 색상으로 돌아가도록 기능 추가
+    // 시도했지만 실패함.
     @IBAction func nextButtonTouched(_ sender: Any) {
-        self.firstLabel.textColor = UIColor.blue
-        self.firstLabel.backgroundColor = UIColor.yellow
-        self.firstLabel.alpha = 0.5
+        if is nextButton.isSelected {
+            self.firstLabel.textColor = UIColor.blue
+            self.firstLabel.backgroundColor = UIColor.yellow
+            self.firstLabel.alpha = 0.5
+        } else {
+            self.firstLabel.textColor = UIColor.white
+            self.firstLabel.backgroundColor = UIColor.purple
+            self.firstLabel.alpha = 1
+        }
     }
 }
 
