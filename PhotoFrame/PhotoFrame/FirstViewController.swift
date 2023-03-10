243928f06fae87c86766202c1aae022975fb9f32
demiiann <tkim0156@gmail.com>
2023-03-11 02:16:20 +0900

    style : Label 변수 이름 변경

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var description: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
-        self.photoLabel.text = "JK의 사진책자"
-        self.photoLabel.textColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).withAlphaComponent(1.0)
-        self.photoLabel.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
-        self.photoLabel.font = UIFont.systemFont(ofSize: 15)
-        self.photoDescription.text = "사진에 대한 설명 Label"
+        self.titleLabel.text = "JK의 사진책자"
+        self.titleLabel.textColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).withAlphaComponent(1.0)
+        self.titleLabel.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        self.titleLabel.font = UIFont.systemFont(ofSize: 15)
+        self.descriptionLabel.text = "사진에 대한 설명 Label"
     }
     
     @IBAction func nextButtonTouched(_ sender: Any) {
-        self.photoLabel.textColor = UIColor.blue
-        self.photoLabel.backgroundColor = UIColor.yellow
-        self.photoLabel.alpha = 0.5
+        self.titleLabel.textColor = UIColor.blue
+        self.titleLabel.backgroundColor = UIColor.yellow
+        self.titleLabel.alpha = 0.5
     }
 }
